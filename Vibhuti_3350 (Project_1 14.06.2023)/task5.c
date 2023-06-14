21a3c906310d10bd6f64ec7570302e653f029e21
VibhuKothiya <135797949+VibhuKothiya@users.noreply.github.com>
2023-06-14 18:42:39 +0530

    Update task5.c

--- Vibhuti_3350 (Project_1 14.06.2023)/task5.c
+++ Vibhuti_3350 (Project_1 14.06.2023)/task5.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+// Write a C program to input electricity unit charges and calculate total electricity bill according to the given condition:
+For first 50 units Rs. 0.50/unit
+For next 100 units Rs. 0.75/unit
+For next 100 units Rs. 1.20/unit
+For unit above 250 Rs. 1.50/unit
+An additional surcharge of 20% is added to the bill.
+	
 	int main(){
 	float unit, total, surcharge, total_bill;
 	
